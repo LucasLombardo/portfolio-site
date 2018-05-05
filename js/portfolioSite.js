9c8a9fc3b98708e9a4e30d82abc7e15c1989582a
Lucas Lombardo <Lucasl@bu.edu>
2018-05-05 12:49:36 -0400

    normalize css

--- js/portfolioSite.js
+++ js/portfolioSite.js
@@ -1,22 +1,57 @@
-window.onload = function addTransitions() {
-	setTimeout(function() {
-		document.querySelector('button').classList.add('loaded');
-	}, 100);
-};
+//add fadein animation after page loaded to prevent flash on page load
+window.onload = setTimeout(function addLoadedClass() {
+	document.querySelector('button').classList.add('loaded');
+}, 20);
 
-//Scroll Logic - NEEDS IMPROVEMENTS
+//sticky nav scroll logic
 
-window.onscroll = function() {
-	myFunction();
-};
-
-var navbar = document.querySelector('.navbar');
-var about = document.querySelector('#about');
-
-function myFunction() {
+window.onscroll = function stickNav() {
+	var navbar = document.querySelector('.navbar');
 	if (window.pageYOffset >= window.innerHeight) {
 		navbar.classList.add('sticky');
 	} else {
 		navbar.classList.remove('sticky');
 	}
+};
+
+//Smooth scroll anchor links
+var targetOffset,
+	currentPosition,
+	body = document.body,
+	button = document.getElementById('smooth-scroll'),
+	animateTime = 900;
+
+function getPageScroll() {
+	var yScroll;
+
+	if (window.pageYOffset) {
+		yScroll = window.pageYOffset;
+	} else if (document.documentElement && document.documentElement.scrollTop) {
+		yScroll = document.documentElement.scrollTop;
+	} else if (document.body) {
+		yScroll = document.body.scrollTop;
+	}
+	return yScroll;
 }
+
+button.addEventListener(
+	'click',
+	function(event) {
+		targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+		currentPosition = getPageScroll();
+
+		body.classList.add('in-transition');
+		body.style.WebkitTransform = 'translate(0, -' + (targetOffset - currentPosition) + 'px)';
+		body.style.MozTransform = 'translate(0, -' + (targetOffset - currentPosition) + 'px)';
+		body.style.transform = 'translate(0, -' + (targetOffset - currentPosition) + 'px)';
+
+		window.setTimeout(function() {
+			body.classList.remove('in-transition');
+			body.style.cssText = '';
+			window.scrollTo(0, targetOffset);
+		}, animateTime);
+
+		event.preventDefault();
+	},
+	false
+);
